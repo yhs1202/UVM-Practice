4709805167948ee6adcaf613da37a62783e77ec8
hsyoon_kccistc <dbsghtmd99@gmail.com>
2025-11-17 01:14:28 +0900

    support saving log file in vcs

--- 251114_TLM/run.sh
+++ 251114_TLM/run.sh
@@ -1,11 +1,12 @@
 # My VCS Build and Simulation Script
 # Author: Hoseung Yoon
-# Last Modified: 2025.11.13
+# Last Modified: 2025.11.17
 
 OUT=sim_out
 
 rm -rf $OUT
 mkdir -p $OUT
+timestamp=$(date +%Y%m%d_%H%M%S)
 
 vcs -full64 -sverilog \
     -ntb_opts uvm-1.2 \
@@ -13,8 +14,10 @@ vcs -full64 -sverilog \
     -timescale=1ns/1ps \
     -Mdir=$OUT/csrc \
     -o $OUT/simv \
+    -l vcs_$timestamp.txt \
     -f run.f
 
 cd $OUT
-./simv +fsdbfile+wave.fsdb
+./simv +fsdbfile+wave.fsdb \
+     -l ../sim_$timestamp.txt
 cd ..
